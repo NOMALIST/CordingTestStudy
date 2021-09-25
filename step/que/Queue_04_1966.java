ee271522c2de61c24139099dda35c40fc3307d38
yojunSeo <yomon926@gmail.com>
2021-09-25 20:57:37 +0900

    주석추가

--- step/que/Queue_04_1966.java
+++ step/que/Queue_04_1966.java
@@ -17,23 +17,30 @@ public class Queue_04_1966 {
 		
 		while(test-- > 0) {
 			
+			// 입력값 받기
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			int n = Integer.parseInt(st.nextToken());
+			// q는 출력순서를 알고자 하는 목표
 			int q = Integer.parseInt(st.nextToken());
 			
 			String[] str = br.readLine().split(" ");
+			// 우선순위 가 들어있는 배열
 			int[] pri = new int[n];
+			// index가 들어있는 큐
 			LinkedList<Integer> que = new LinkedList<Integer>();
 			
+			// que에 출력순서를 저장
 			for(int j=0; j<n; j++) {
 				pri[j] = Integer.parseInt(str[j]);
 				que.offer(j);
 			}
 			
 			int ans = -1;
+			// j는 프린트 하는 순서
 			for(int j=1; j<=n; j++) {
 				int maxPri = -1;
 				int maxIndex = -1;
+				// 큐를 size만큼 돌면서 max값 찾기
 				for(int k=0; k<que.size(); k++) {
 					if(maxPri < pri[que.peek()]) {
 						maxPri = pri[que.peek()];
@@ -43,6 +50,7 @@ public class Queue_04_1966 {
 					que.offer(que.poll());
 				}
 				
+				// max 값이 목표물이면 현재 번호를 출력
 				if(maxIndex == q) {
 					ans = j;
 					break;
