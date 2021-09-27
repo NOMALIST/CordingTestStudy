337a2a8b45f57e50a6d6c20690433cf11248b794
yojunSeo <yomon926@gmail.com>
2021-09-27 23:49:44 +0900

    09/27 DP 공부

---
+++ setp/DP/DP_01_1003.java
@@ -0,0 +1,31 @@
+package setp.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DP_01_1003 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		int[][] arr = new int[41][2];
+		arr[0][0] = 1;
+		arr[1][1] = 1;
+		
+		for(int i=2; i<=40; i++) {
+			arr[i][0] = arr[i-1][0] + arr[i-2][0];
+			arr[i][1] = arr[i-1][1] + arr[i-2][1];
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		while(T-- > 0) {
+			int N = Integer.parseInt(br.readLine());
+			sb.append(arr[N][0] + " " + arr[N][1] + "\n");
+		}
+		
+		System.out.println(sb);
+		br.close();
+	}
+}
